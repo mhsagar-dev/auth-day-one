bd788c2bcc66470a0e944eaa784deb9b360d7118
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-12-20 16:37:50 +0600

    added signout button : :

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { useState } from "react";
 import initializeAuthentication from "./Firebase/firebase.initialize";
 initializeAuthentication();
 
 const GoogleProvider = new GoogleAuthProvider();
+const auth = getAuth();
 export default function App() {
   const [user, setUser] = useState('');
   const HandleGoogleSignIn = () => {
-    const auth = getAuth();
     signInWithPopup(auth, GoogleProvider)
       .then((result) => {
         const user = result.user;
         setUser(user);
       })
   }
+  const HandleSignOut = () => {
+    signOut(auth).then(() => {
+      alert('Signed Out successfully');
+    }).catch((error) => {
+      // An error happened.
+    });
+  }
   return (
     <div className="App">
-      {user?.email ? <button >Logout</button> : <button onClick={HandleGoogleSignIn}>Sign In With Google</button>}
+      {user?.email ? <button onClick={HandleSignOut}>Logout</button> : <button onClick={HandleGoogleSignIn}>Sign In With Google</button>}
       {user?.email && <div>
         <h1>Hello {user.displayName}</h1>
       </div>}
