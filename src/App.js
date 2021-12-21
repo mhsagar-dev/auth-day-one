3d822ad6a1c0a8cb655eae62f1edf0216ddc1df2
unknown <mhsagar.dev@gmail.com>
2021-12-21 18:35:02 +0600

    first day of firebase authentication for beginers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import {getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import React, { useState } from 'react';
+import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider, signOut } from "firebase/auth";
 import './App.css';
 import initializeAuthentication from "./Firebase/firebase.initialize";
 
@@ -6,20 +7,72 @@ import initializeAuthentication from "./Firebase/firebase.initialize";
 
 initializeAuthentication();
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+
 
 function App() {
-  const handleGoogleSignIn = () =>{
-    const auth = getAuth();
-    signInWithPopup(auth, provider)
-    .then((result) => {
-      const user = result.user;
-      console.log(user);
+
+  const [user, setUser] = useState({})
+  const auth = getAuth();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        const { displayName, email, photoURL } = result.user;
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        };
+        setUser(loggedInUser);
+      })
+      .catch(error => {
+        console.log(error.message);
+      })
+  }
+
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then(result => {
+        const {displayName, photoURL, email} = result.user;
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        }
+        setUser(loggedInUser);
+      }).catch((error) => {
+        
+      });
+  }
+
+  const handleSignOut = () => {
+    signOut(auth)
+    .then(() => {
+      setUser({})
     })
   }
+
   return (
     <div className="App">
-      <button onClick={ handleGoogleSignIn }>Google</button>
+      
+      { !user.name ?
+        <div>
+        <button onClick={handleGoogleSignIn}>Google</button>
+        <button onClick={handleGithubSignIn}>github</button>
+        </div> :
+        <button onClick={handleSignOut}>SignOut</button>
+      }
+      
+      <br />
+      {
+        user.name && <div>
+          <h2>Welcome {user.name}</h2>
+          <p>your email {user.email}</p>
+          <img src={user.photo} alt="" />
+        </div>
+      }
     </div>
   );
 }
--- src/Firebase/firebase.initialize.js
+++ src/Firebase/firebase.initialize.js
@@ -1,10 +1,10 @@
 import firebaseConfig from './firebase.config';
-import { initializeApp } from '/firebase/app';
+import { initializeApp } from 'firebase/app';
 
 
 const initializeAuthentication = () => {
     initializeApp(firebaseConfig);
 }
 
-export default initializeAuthentication;
+export default initializeAuthentication; 
 
