0b6da56c325f35dd45f443cbff46cd156ee1b447
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-12-21 12:12:35 +0600

    added user , pic & removed unnecesary file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
-import { useState } from "react";
+import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
+import { useEffect, useState } from "react";
 import initializeAuthentication from "./Firebase/firebase.initialize";
 initializeAuthentication();
 
@@ -8,23 +8,24 @@ const auth = getAuth();
 export default function App() {
   const [user, setUser] = useState('');
   const HandleGoogleSignIn = () => {
-    signInWithPopup(auth, GoogleProvider)
-      .then((result) => {
-        const user = result.user;
-        setUser(user);
-      })
+    signInWithPopup(auth, GoogleProvider).then(result => { setUser(user) }).then(error => { });
   }
   const HandleSignOut = () => {
-    signOut(auth).then(() => {
-      alert('Signed Out successfully');
-    }).catch((error) => {
-      // An error happened.
-    });
+    signOut(auth).then(result => { setUser('') }).then(error => { });
   }
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+      }
+    });
+  }, [])
   return (
     <div className="App">
       {user?.email ? <button onClick={HandleSignOut}>Logout</button> : <button onClick={HandleGoogleSignIn}>Sign In With Google</button>}
       {user?.email && <div>
+        <img style={{ borderRadius: '50%', marginTop:'20px' }} src={user.photoURL} alt="profile" />
         <h1>Hello {user.displayName}</h1>
       </div>}
     </div>
