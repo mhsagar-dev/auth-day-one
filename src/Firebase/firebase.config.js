c0f9c6dae96034b945a80c500594957636586936
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-12-21 12:49:58 +0600

    added .env file

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,12 +1,13 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+console.log(process.env);
 const firebaseConfig = {
-  apiKey: "AIzaSyBftAqaPztIJMOXn7HTFvZQeekclJfL_1M",
-  authDomain: "user-post-admin.firebaseapp.com",
-  projectId: "user-post-admin",
-  storageBucket: "user-post-admin.appspot.com",
-  messagingSenderId: "374987762210",
-  appId: "1:374987762210:web:d5b29dabbbe61d1facf87b",
-  measurementId: "G-CT9YTZXLDM"
+  apiKey:process.env.REACT_APP_API_KEY,
+  authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+  projectId:process.env.REACT_APP_PROJECT_ID,
+  storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId:process.env.REACT_APP_APP_ID,
+  measurementId:process.env.REACT_APP_MEASUREMENT_ID
 };
 
 export default firebaseConfig;
